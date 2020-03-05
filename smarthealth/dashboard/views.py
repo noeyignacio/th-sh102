3e2d94bc2ea12b4f1379ebafaa35923cdbab66a8
Noey <ignacionooey@gmail.com>
2020-03-05 11:46:46 +0800

    Dashboard

--- smarthealth/dashboard/views.py
+++ smarthealth/dashboard/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+
+from datetime import date, datetime
+from django.db.models.functions import TruncMonth, TruncDate
+from django.db.models import Count
+from time import strftime
+
 from patients.models import Patient
 
 def doctorsDashboardHospital(request):
 
-    return render(request, 'doctors/views/doctors_dashboard_hospital.html')
+    patient = Patient.objects.all()
+
+    today = date.today().strftime('%m')
+    totalPatientsTodaysMonth = Patient.objects.annotate(month=TruncMonth('date_joined'))\
+            .values('month').annotate(total=Count('date_joined')).filter(date_joined__month=today)
+
+    total_patient = patient.count()
+
+    context = {
+        'patient' : patient,
+        'total_patient' : total_patient,
+        'totalPatientsTodaysMonth': totalPatientsTodaysMonth
+    }
+
+    return render(request, 'doctors/views/doctors_dashboard_hospital.html', context)
 
 
 def doctorsDashboardAnalytics(request):
--- smarthealth/nurses/models.py
+++ smarthealth/nurses/models.py
@@ -1,3 +1,43 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+class Nurse(models.Model):
+
+
+    GENDER_CHOICES = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+        ('Prefer not to say', 'Prefer not to say')
+    )
+
+    STATUS = (
+        ('Single', 'Single'),
+        ('Married', 'Married'),
+        ('Divorced', 'Divorced'),
+        ('Legally Seperated', 'Legally Seperated'),
+        ('Widowed', 'Widowed')
+    )
+
+    WARD_CHOICES = (
+        ('SH-102', 'SH-102'),
+        ('SH-101', 'SH-101'),
+        ('SH-201', 'SH-201'),
+    )
+
+
+    username = models.CharField(max_length=100, null=True, default="N/A")
+    lastName = models.CharField(max_length=100, null=True, default="N/A")
+    firstName = models.CharField(max_length=100, null=True, default="N/A")
+    middleName = models.CharField(max_length=100, null=True, default="N/A")
+    birthDate = models.CharField(max_length=100, null=True, default="N/A")
+    gender = models.CharField(max_length=20, choices=GENDER_CHOICES, blank=False)
+    maritalStatus = models.CharField(max_length=20, null=True, choices=STATUS, blank=True)
+    mobile_number = models.CharField(max_length=11, null=True,blank=False, validators=[RegexValidator(r'^\d{10,11}$')])
+    email = models.EmailField(max_length=100, null=True)
+    ward = models.CharField(max_length=10, choices=WARD_CHOICES, blank=True)
+    
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.lastName + ', ' + self.firstName + ' ' + self.middleName
